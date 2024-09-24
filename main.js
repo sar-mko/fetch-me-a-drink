8f3ef017948bcc33b54ef8590e8991cfc67d5dbb
Sarah Meko <tmekos1@gmail.com>
2024-09-23 18:19:25 -0700

    incorporate drink data every search

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 const buttonsDivs = document.querySelector('.button-listen').addEventListener('click', getByName )
+const drinkName = document.querySelector('.drinkName')
+const drinkCategory = document.querySelector('#drinkCategory')
+const drinkGlassWare = document.querySelector('#drinkGlassware')
+const drinkType = document.querySelector('#drinkType')
+
 async function getByName(e){
     try{
         const val = document.querySelector('#drinkSearch').value.trim();
         const res = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${val}`)
         const data = await res.json()
-        console.log(data)
+        console.log(data.drinks[0])
+        const firstDrink = data.drinks[0]
+
+        drinkName.innerHTML = firstDrink.strDrink
+        drinkCategory.innerHTML = firstDrink.strCategory
+        drinkType.innerHTML = firstDrink.strAlcoholic
+        drinkGlassWare.innerHTML = firstDrink.strGlass
     }
    catch(err){
     console.log(`Error: ${err}`)
