1c27648188b57cb48c1fd4b3abfbdbb80d19d4c8
Sarah Meko <tmekos1@gmail.com>
2024-10-27 12:08:32 -0700

    check for empty values

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ const drinkIngredients = document.querySelector('#drinkIngredients')
 async function getByName(e){
     try{
         const val = document.querySelector('#drinkSearch').value.trim();
+        if(!val){
+            alert('Please enter a value') 
+            return;
+        }
         const res = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${val}`)
         const data = await res.json()
         console.log(data.drinks[0])
