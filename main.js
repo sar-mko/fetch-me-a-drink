5209b8430fd615c6d9887f47e4c5d3018454bc73
Sarah Meko <tmekos1@gmail.com>
2024-11-03 17:53:11 -0800

    clean console.logs

--- main.js
+++ main.js
@@ -25,20 +25,17 @@ async function getByName(e){
         populateScreen(data,0)
         getStoreList(data,0)
         let num = 1
-
     }
    catch(err){
     console.log(`Error: ${err}`)
    }
 }
 
-// const input = document.querySelector('input[name="drinkSearch"]');
 
 async function getRandom(){
     try{
         const res = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
         const data = await res.json()
-        // console.log(data.drinks[0])
 
         populateScreen(data,0)
         getStoreList(data, 0)
@@ -49,191 +46,43 @@ async function getRandom(){
 }
 
 async function rotate(direction){
-    //change to goLeft
     try{
-        // if already a selected drink(ex. we clicked random and the drink in front is martini, start searching from the m drink list)
-        //if no drink selected, start from A
-        // and once a is done, got to b? or just recycle the list of A
-        console.log('HERE!')
         let data = null
         if(!currentDrink){
             const res = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a`)
             data = await res.json()
             getStoreList(data,0)
             populateScreen(rotatingList, currentIndex)
-            console.log('here, 2!!!!!')
             return
         }else if(rotatingList.drinks.length === 1){
             const res = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${currentDrink[0]}`)
             data = await res.json()
             getStoreList(data,0)
             populateScreen(rotatingList, currentIndex)
-            console.log('here, 3!!!!!')
             return
         }
         if(direction === 'left'){
             if(currentIndex == 0){
-                console.log( 'HERRRR', currentIndex)
                  currentIndex = rotatingList.drinks.length - 1
              }else{
-                 console.log('nooooooo')
                  currentIndex--
              }
-             console.log('here, 4!!!!!')
         }else if(direction ==='right'){
             if(currentIndex >= rotatingList.drinks.length - 1){
-                console.log( 'HERRRR', currentIndex)
                  currentIndex = 0
              }else{
-                 console.log('nooooooo')
                  currentIndex++
              }
-             console.log('here, 5!!!!!')
         }
-        populateScreen(rotatingList, currentIndex)
-        console.log('here, 6!!!!!')
-        
-        console.log('LENGTH!', rotatingList, 'len',rotatingList.drinks.length,'currI',currentIndex )
-        // const firstDrink = data.drinks[0]
-        // currentDrink = firstDrink.strDrink
-        // rotatingList = data
-        // listIndex = 0
-
-        // console.log(data.drinks[0].strDrink[0])
-        // const newList = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${data.drinks[0].strDrink[0]}`)
-        // console.log(data.drinks[0])
-        // console.log(data)
-        /**
-         * if list is only one item, do goRandom() request (or alert first who cares)
-         * 
-         * if at the end of the list, go to list[0]
-         * 
-         * else go to the next i + 1
-         * (so do i store the index its at?)
-         * 
-         * if 
-         *
-         * 
-         */
-
+        populateScreen(rotatingList, currentIndex)        
     }catch(err){
         console.log(err)
     }
 }
-
-
-async function goLeft(){
-    //change to goLeft
-    try{
-        // if already a selected drink(ex. we clicked random and the drink in front is martini, start searching from the m drink list)
-        //if no drink selected, start from A
-        // and once a is done, got to b? or just recycle the list of A
-        
-        let data = null
-        if(!currentDrink){
-            const res = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a`)
-            data = await res.json()
-            getStoreList(data,0)
-        }else if(rotatingList.length === 1){
-            const res = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${currentDrink[0]}`)
-            data = await res.json()
-            getStoreList(data,0)
-        }else if(currentIndex == 0){
-           console.log( 'HERRRR', currentIndex)
-            currentIndex = rotatingList.drinks.length - 1
-        }else{
-            console.log('nooooooo')
-            currentIndex--
-        }
-        console.log('LENGTH!', rotatingList, 'len',rotatingList.drinks.length,'currI',currentIndex )
-        // const firstDrink = data.drinks[0]
-        // currentDrink = firstDrink.strDrink
-        // rotatingList = data
-        // listIndex = 0
-
-        populateScreen(rotatingList, currentIndex)
-        // console.log(data.drinks[0].strDrink[0])
-        // const newList = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${data.drinks[0].strDrink[0]}`)
-        // console.log(data.drinks[0])
-        // console.log(data)
-        /**
-         * if list is only one item, do goRandom() request (or alert first who cares)
-         * 
-         * if at the end of the list, go to list[0]
-         * 
-         * else go to the next i + 1
-         * (so do i store the index its at?)
-         * 
-         * if 
-         *
-         * 
-         */
-
-    }catch(err){
-        console.log(err)
-    }
-}
-
-//go left or right, pass in left or pass in right?
-async function goRight(){
-    //change to goLeft
-    try{
-        // if already a selected drink(ex. we clicked random and the drink in front is martini, start searching from the m drink list)
-        //if no drink selected, start from A
-        // and once a is done, got to b? or just recycle the list of A
-        let data = null
-        if(!currentDrink){
-            const res = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a`)
-            data = await res.json()
-            getStoreList(data,0)
-        }else if(rotatingList.length === 1){
-            const res = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${currentDrink[0]}`)
-            data = await res.json()
-            getStoreList(data,0)
-        }else if(currentIndex >= rotatingList.drinks.length - 1){
-           console.log( 'HERRRR', currentIndex)
-            currentIndex = 0
-        }else{
-            console.log('nooooooo')
-            currentIndex++
-        }
-        console.log('LENGTH!', rotatingList, 'len',rotatingList.drinks.length,'currI',currentIndex )
-        // const firstDrink = data.drinks[0]
-        // currentDrink = firstDrink.strDrink
-        // rotatingList = data
-        // listIndex = 0
-
-        populateScreen(rotatingList, currentIndex)
-        // console.log(data.drinks[0].strDrink[0])
-        // const newList = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${data.drinks[0].strDrink[0]}`)
-        // console.log(data.drinks[0])
-        // console.log(data)
-        /**
-         * if list is only one item, do goRandom() request (or alert first who cares)
-         * 
-         * if at the end of the list, go to list[0]
-         * 
-         * else go to the next i + 1
-         * (so do i store the index its at?)
-         * 
-         * if 
-         *
-         * 
-         */
-
-
-    }catch(err){
-        console.log(err)
-    }
-}
-
 
 function populateScreen(data,index){
-    console.log(data)
     const firstDrink = data.drinks[index]
     currentDrink = firstDrink.strDrink
-    console.log('CURRENT DRINK',currentDrink, currentDrink[0])
-
     
     drinkName.innerHTML = firstDrink.strDrink
     drinkCategory.innerHTML = firstDrink.strCategory
@@ -244,8 +93,8 @@ function populateScreen(data,index){
     drinkImg.src = firstDrink.strDrinkThumb
 
     let num = 1
+
     while(firstDrink[`strIngredient${num}`] !== null){
-        console.log(firstDrink[`strIngredient${num}`])
         const ingredient = document.createElement('li')
         drinkIngredients.append(firstDrink[`strIngredient${num}`], ingredient)
         num++
@@ -254,11 +103,9 @@ function populateScreen(data,index){
 }
 
 function getStoreList(data, index){
-    const firstDrink = data.drinks[0]
+    const firstDrink = data.drinks[index]
     currentDrink = firstDrink.strDrink
     rotatingList = data
     listIndex = index
     currentIndex = index
-    console.log('stored!')
-    console.log('curr:',currentDrink , 'rotatingList:', rotatingList, 'listIndex', listIndex)
 }
